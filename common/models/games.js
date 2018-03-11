f6e1d7b35ae2621fca8fbe92f3ce04fd8c88b6ca
oneaaronsmith <emailofaaron62@gmail.com>
2018-03-11 13:06:53 -0500

    Fixed scores issue and introduced game remote hook for creating 18 scores

--- common/models/games.js
+++ common/models/games.js
@@ -1,1 +1,16 @@
-'use strict'; module.exports = function(Games) {};
+'use strict'; module.exports = function(Games) {
+
+    Games.afterRemote('create', function(context, remoteMethodOutput, next) {
+        console.log('AYYYY LMAO');
+        console.log(remoteMethodOutput);
+
+        var ds = Games.dataSource;
+        var sql = 'INSERT INTO scores (hole_id, game_id) SELECT hole_id,'+ remoteMethodOutput.gameId +' FROM holes WHERE course_id = ' + remoteMethodOutput.courseId + '';
+       
+        ds.connector.query(sql, function(err, scores) {
+            if (err) console.error(err);
+        });
+
+        next();
+    });
+};
