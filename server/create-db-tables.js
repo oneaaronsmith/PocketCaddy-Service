20a33a2c92851476501039558d0628299e493a7e
oneaaronsmith <emailofaaron62@gmail.com>
2018-04-10 08:12:33 -0500

    Authentication working - fixed issue with scopes

---
+++ server/create-db-tables.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+var server = require('./server');
+
+/*var ds = loopback.createDataSource('mysql', {
+  "host": process.env.DB_HOST,
+  "port": 3306,
+  "database": process.env.DB_NAME,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS
+});*/
+
+var ds = require('./datasources.production.js');
+var ds = server.dataSources.pocketcaddy_np;
+
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
--- server/datasources.production.js
+++ server/datasources.production.js
@@ -9,5 +9,4 @@ module.exports = {
       "user": process.env.DB_USER,
       "connector": "mysql"
     }
-    
 }
